f1b1f3b83463fd34990bbe6523fea4971af9a427
zaldibaik <zaldi104@smk.belajar.id>
2024-05-06 11:30:08 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <div className="card">
           <div className="container">
-            <h4><b>halo saya Zaldi</b></h4>
-          
+            <h4>
+              <b>halo saya Zaldi</b>
+            </h4>
+          </div>
+          <footer>
+            <p>belajar JS</p>
+          </footer>
         </div>
-        <footer>
-        <p>belajar JS</p>
-      </footer>
-      </div>
+        <div className="card2">
+          <h4>
+            <b>halo saya Zaldi</b>
+          </h4>
 
+          <footer>
+            <tr>
+              <ul>
+                <li>nama</li>
+                <li>Email</li>
+                <li>batc Pelatihan</li>
+              </ul>
+            </tr>
+          </footer>
+        </div>
       </header>
-      
     </div>
   );
 }
