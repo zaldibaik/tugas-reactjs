8285e31a2906beec7b22374bdfb8094767ee5b7e
zaldibaik <zaldi104@smk.belajar.id>
2024-05-06 15:48:29 +0700

    Update

--- src/tugas9.js
+++ src/tugas9.js
@@ -2,35 +2,61 @@ import React from "react";
 class Tugas9 extends React.Component {
   render() {
     return (
-
-<table class="table-auto">
-  <thead>
-    <tr>
-      <th>Song</th>
-      <th>Artist</th>
-      <th>Year</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-      <td>Malcolm Lockyer</td>
-      <td>1961</td>
-    </tr>
-    <tr>
-      <td>Witchy Woman</td>
-      <td>The Eagles</td>
-      <td>1972</td>
-    </tr>
-    <tr>
-      <td>Shining Star</td>
-      <td>Earth, Wind, and Fire</td>
-      <td>1975</td>
-    </tr>
-  </tbody>
-</table>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>age</th>
+            <th>action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Zaldi</td>
+            <td>Malcolm Lockyer</td>
+            <td>
+              <button>
+                <a>hapus</a>
+              </button>
+            </td>
+            <td>
+              <button>
+                <a>Edit</a>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>Zaldi</td>
+            <td>Malcolm Lockyer</td>
+            <td>
+              <button>
+                <a>hapus</a>
+              </button>
+            </td>
+            <td>
+              <button>
+                <a>Edit</a>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>Zaldi</td>
+            <td>Malcolm Lockyer</td>
+            <td>
+              <button>
+                <a>hapus</a>
+              </button>
+            </td>
+            <td>
+              <button>
+                <a>Edit</a>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     );
-}
+  }
 }
 
 export default Tugas9;
